50a0f2a6d121a0412fcf451255a36afe95239fde
Alex Nguyen <hoanghai92@hotmail.com>
2020-04-18 19:16:27 +0200

    Attack animation stops run animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 10.0f;
-    public float rotateSpeed = 30f;
+    public float rotateSpeed = 0.15f;
 
     public float horizontalInput;
     public float verticalInput;
@@ -16,14 +16,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("init");
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         // transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * movementSpeed);
@@ -35,23 +33,38 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Debug.Log("movement" + movement);
+        if (animator.GetBool("isAttacking") == false)
+        {
+            Debug.Log(animator.GetBool("isAttacking"));
+            animator.SetBool("isRunning", false);
+            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotateSpeed);
+        }
+
         if (movement != Vector3.zero)
         {
             animator.SetBool("isRunning", true);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
         else
         {
             animator.SetBool("isRunning", false);
         }
 
-
-        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log(animator.enabled);
             Instantiate(projecttilePrefab, new Vector3(transform.position.x, projecttilePrefab.transform.position.y, transform.position.z), projecttilePrefab.transform.rotation);
+            animator.SetBool("isAttacking", true);
         }
+
+        // if (Input.GetKeyUp(KeyCode.Space))
+        // {
+        //     animator.enabled = false;
+        //     animator.SetBool("isAttacking", true);
+
+        // }
+
+
+
     }
 }
