cb66933a9936ab97f63f4c95caa2f7bcc5f8c37b
Thong Trong Ho <norbadboy@gmail.com>
2020-04-18 18:36:36 +0200

    add spawn manager, destroy objects out of borders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     public GameObject projecttilePrefab;
 
+    public float xRangeleft = 18f;
+    public float xRangeright = 23.0f;
+    private float zRange = 32.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +55,26 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projecttilePrefab, new Vector3(transform.position.x, projecttilePrefab.transform.position.y, transform.position.z), projecttilePrefab.transform.rotation);
+        }
+        // Player move in range
+        if (transform.position.x < -xRangeleft)
+        {
+            transform.position = new Vector3(-xRangeleft, transform.position.y, transform.position.z);
+
+        }
+        if (transform.position.x > xRangeright)
+        {
+            transform.position = new Vector3(xRangeright, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
         }
     }
 }
