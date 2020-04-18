a8097f2585f26e91c2d1ce0af9056746988a2db5
norbadboy <63293007+norbadboy@users.noreply.github.com>
2020-04-18 18:41:50 +0200

    add spawn manager, destroy objects out of borders (#1)

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] girlsPrefabs;
+    private float spawnRangeX = 25;
+    private float spawnPosZ = 33;
+    private float startDelay = 1;
+    private float spawnInterval = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawn mæber randomly
+        InvokeRepeating("SpawnRandomGirls", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Spawn random mæber at random x position at end of play area
+    void SpawnRandomGirls()
+    {
+        //Generate random mæber index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        // instantiate animal at random spawn location
+        int girlsIndex = Random.Range(0, girlsPrefabs.Length);
+        Instantiate(girlsPrefabs[girlsIndex], spawnPos, girlsPrefabs[girlsIndex].transform.rotation);
+
+    }
+
+}
