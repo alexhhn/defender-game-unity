fed74c1ffbb981220bbc8499fde607bc8ba130f5
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2020-04-18 20:00:30 +0200

    Feature/attack animation (#2)
    * Add animation controller
    
    * Attack animation stops run animation
    
    * Remove redundant codes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 10.0f;
-    public float rotateSpeed = 30f;
+    public float rotateSpeed = 0.15f;
 
     public float horizontalInput;
     public float verticalInput;
@@ -19,18 +19,14 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("init");
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        // transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * movementSpeed);
-        // transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * movementSpeed);
 
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
@@ -38,23 +34,28 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Debug.Log("movement" + movement);
+        if (animator.GetBool("isAttacking") == false)
+        {
+            Debug.Log(animator.GetBool("isAttacking"));
+            animator.SetBool("isRunning", false);
+            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotateSpeed);
+        }
+
         if (movement != Vector3.zero)
         {
             animator.SetBool("isRunning", true);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
         else
         {
             animator.SetBool("isRunning", false);
         }
 
-
-        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log(animator.enabled);
             Instantiate(projecttilePrefab, new Vector3(transform.position.x, projecttilePrefab.transform.position.y, transform.position.z), projecttilePrefab.transform.rotation);
+            animator.SetBool("isAttacking", true);
         }
         // Player move in range
         if (transform.position.x < -xRangeleft)
---
+++ Assets/Scripts/StopAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAttack : StateMachineBehaviour
+{
+    private float timePassed = 0;
+
+    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetBool("isAttacking") == true)
+        {
+            timePassed += Time.deltaTime;
+            if (timePassed < stateInfo.length) return;
+
+            animator.SetBool("isAttacking", false);
+        }
+        else
+        {
+            timePassed = 0;
+        }
+
+    }
+}
