76a60952c52ab53b81cd8ae55f6b3d335b8f2995
Alex Nguyen <hoanghai92@hotmail.com>
2020-04-18 19:07:52 +0200

    Add animation controller

---
+++ Assets/StopRun.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRun : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+    private float timePassed = 0;
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+
+        // Debug.Log(timePassed);
+
+        // while timePassed is smaller than the sanimation clip's length
+        // do nothing
+
+        // reset the bool
+        // 
+        // animator.enabled = false;
+        if (animator.GetBool("isRunning") == true)
+        {
+            timePassed += Time.deltaTime;
+            if (timePassed < stateInfo.length) return;
+
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            timePassed = 0;
+        }
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
