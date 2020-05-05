9dea38396d580d9a669bf7cd4b548d0dfc8a5c76
norbadboy <63293007+norbadboy@users.noreply.github.com>
2020-05-05 15:43:34 +0200

    add force landscape (#8)

---
+++ Assets/GroundGameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Enemy")
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
