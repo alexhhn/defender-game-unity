34de06b4c9aaecd94417028683b44b299dc415ea
Thong Trong Ho <norbadboy@gmail.com>
2020-05-22 21:17:33 +0200

    Add random spawn power ups

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,14 +6,28 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 4.5f;
     private float spawnPosZ = 5.5f;
     private float startDelay = 1;
-    private float spawnInterval = 1;
+    private float spawnInterval = 0;
     private int lastSpawnIndex = 0;
+
+    //Spawn power-ups
+    public GameObject[] powerupsPrefabs;
+    private float spawnPowerRangeX = 4.5f;
+    public float spawnPowerPosZ = -3f;
+    public float startDelayPower = 1;
+    public float spawnPowerInterval = 5;
+
+    //Player
+    public GameObject Player;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Spawn mæber randomly
         InvokeRepeating("SpawnRandomGirls", startDelay, spawnInterval);
+
+        //Spawn power-ups
+        InvokeRepeating("SpawnPowerUps", startDelayPower, spawnPowerInterval);
     }
 
     // Update is called once per frame
@@ -28,7 +42,7 @@ public class SpawnManager : MonoBehaviour
         //Generate random mæber index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-        // instantiate animal at random spawn location
+        // instantiate mæber at random spawn location
         int girlsIndex = Random.Range(0, girlsPrefabs.Length);
 
         // Decrease the chance of spawning enemy at the same position
@@ -41,5 +55,42 @@ public class SpawnManager : MonoBehaviour
         girlsIndex = lastSpawnIndex;
 
     }
+    void SpawnPowerUps()
+    {
+        float playerPosition = Player.transform.position.x;
+        // hvis x er 2
+        // -4.5 
 
+        float leftPos = Random.Range(-spawnPowerRangeX, playerPosition - 1);
+        float rightPos = Random.Range(playerPosition + 1, spawnPowerRangeX);
+        // Debug.Log(leftPos + "left");
+        // Debug.Log(rightPos + "right");
+        //Generate random power-ups index and random spawn position
+        float random = Random.Range(0, 100);
+
+        Debug.Log(random + "random");
+
+        Vector3 spawnPosPowerups;
+        if (random > 50)
+        {
+            spawnPosPowerups = new Vector3(leftPos, 0, spawnPowerPosZ);
+        }
+        else
+        {
+            spawnPosPowerups = new Vector3(rightPos, 0, spawnPowerPosZ);
+        }
+
+        // instantiate mæber at random spawn location
+        int powerupsIndex = Random.Range(0, powerupsPrefabs.Length);
+
+        // Decrease the chance of spawning enemy at the same position
+        if (powerupsIndex == lastSpawnIndex)
+        {
+            powerupsIndex = Random.Range(0, powerupsPrefabs.Length);
+        }
+
+        Instantiate(powerupsPrefabs[powerupsIndex], spawnPosPowerups, powerupsPrefabs[powerupsIndex].transform.rotation);
+        powerupsIndex = lastSpawnIndex;
+
+    }
 }
